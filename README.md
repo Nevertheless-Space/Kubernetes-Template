85e9498b66b7e8dfa2a1c9fa82a99c92763d8552
neverthelessspace <nevertheless.space@gmail.com>
2021-04-01 01:12:00 +0200

    nginx-controllers

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # Kubernetes-Template
-Kubernetes Template
+Kubernetes as Code Template
+
+## Local Debug
+### GitLab/Http Terraform Backend
+
+```powershell
+$username = "XXXXXXXXXX"
+$password = "XXXXXXXXXXXXXXX"
+$project_id = "XXXXXXXXX"
+$tfstate_name = "<PROJECT-NAME>"
+
+terraform init `
+    -backend-config="address=https://gitlab.com/api/v4/projects/$project_id/terraform/state/$tfstate_name" `
+    -backend-config="lock_address=https://gitlab.com/api/v4/projects/$project_id/terraform/state/$tfstate_name/lock" `
+    -backend-config="unlock_address=https://gitlab.com/api/v4/projects/$project_id/terraform/state/$tfstate_name/lock" `
+    -backend-config="username=$username" `
+    -backend-config="password=$password" `
+    -backend-config="lock_method=POST" `
+    -backend-config="unlock_method=DELETE" `
+    -backend-config="retry_wait_min=5"
+```
+
+### Terraform minimum variables
+```bash
+terraform plan --var subscription_id=XXXXXXXXXXXXXXXXXXXXXX --var client_id=XXXXXXXXXXXXXXXXXXXXXX --var client_secret=XXXXXXXXXXXXXXXXXXXXXX --var tenant_id=XXXXXXXXXXXXXXXXXXXXXX --var tenant=example --var env=test
+
+terraform apply --var subscription_id=XXXXXXXXXXXXXXXXXXXXXX --var client_id=XXXXXXXXXXXXXXXXXXXXXX --var client_secret=XXXXXXXXXXXXXXXXXXXXXX --var tenant_id=XXXXXXXXXXXXXXXXXXXXXX --var tenant=example --var env=test
+
+terraform destroy --var subscription_id=XXXXXXXXXXXXXXXXXXXXXX --var client_id=XXXXXXXXXXXXXXXXXXXXXX --var client_secret=XXXXXXXXXXXXXXXXXXXXXX --var tenant_id=XXXXXXXXXXXXXXXXXXXXXX --var tenant=example --var env=test
+```
---
+++ k8s.apps.nginx-controller.tf
@@ -0,0 +1,69 @@
+locals{
+  k8s_apps_nginx_controller_enabled = true
+  k8s_apps_nginx_controller_depends_on = module.aks.kube_config
+
+  k8s_apps_nginx_controller_namespace = "nginx-controllers"
+  k8s_apps_nginx_controller_version = "2.11.3"
+
+  k8s_apps_nginx_controller_specs = [
+    {
+      name = "test"
+      ingress_class = "nginx-test"
+      enable_http = false
+      enable_https = true
+      log_format = "[$time_local] $remote_addr - $remote_user \"$request\" $status $body_bytes_sent \"$http_referer\" \"$http_user_agent\" \"$http_x_forwarded_for\" \"$host\" \"$http_x_forwarded_host\" \"$http_x_forwarded_proto\""
+      other_configurations = [
+        {
+          name = "controller.config.proxy-buffer-size"
+          value = "16k"
+        },
+        {
+          name = "controller.config.proxy-buffers"
+          value = "4 16k"
+        },
+        {
+          name = "proxy-busy-buffers-size"
+          value = "16k"
+        },
+        {
+          name = "use-forwarded-headers"
+          value = "true"
+        }
+      ]
+      annotations = [
+        {
+          name = "test.annotation"
+          value = "example"
+        }
+      ]
+    },
+  ]
+}
+resource "kubernetes_namespace" "ingress_controller" {
+
+  provider = kubernetes.default
+  depends_on = [local.k8s_apps_nginx_controller_depends_on]
+  count = local.k8s_apps_nginx_controller_enabled == true ? 1 : 0
+
+  metadata {
+    name = local.k8s_apps_nginx_controller_namespace
+    labels = {
+      name = local.k8s_apps_nginx_controller_namespace
+    }
+  }
+}
+module "ingress_controller" {
+
+  providers = {
+    helm = helm.default
+  }
+  
+  depends_on = [kubernetes_namespace.ingress_controller]
+  count = local.k8s_apps_nginx_controller_enabled == true ? 1 : 0
+
+  source = "git::https://github.com/nevertheless-space/terraform-modules//kubernetes/ingress/nginx-controller?ref=kubernetes/ingress/nginx-controller-1.0.0"
+  
+  namespace = local.k8s_apps_nginx_controller_namespace
+  chart_version = local.k8s_apps_nginx_controller_version
+  ingress_controllers = local.k8s_apps_nginx_controller_specs
+}
